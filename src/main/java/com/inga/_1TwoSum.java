cb45fff13308f4e1edca0eda033836680d6dbc52
abing0610 <1500811059@qq.com>
2016-10-31 21:54:19 +0800

    project init

---
+++ src/main/java/com/inga/_1TwoSum.java
@@ -0,0 +1,48 @@
+package com.inga;
+
+import com.sun.tools.corba.se.idl.ExceptionEntry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by abing on 2016-10-31.
+ */
+public class _1TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,5};
+//        int [] ss =  twoSum(nums , 6);
+        int[]  ss = twoSum2(nums , 10);
+        for (int i = 0 ; i < ss.length ;i++){
+            System.out.print(ss[i] + " ");
+        }
+
+    }
+
+
+
+    public static int[] twoSum2(int[] nums , int target){
+        Map<Integer , Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0 ; i < nums.length ; i++){
+            if (map.containsKey( target - nums[i])) {
+                return  new int[]{map.get(target - nums[i])  + 1  , i+1};
+            }
+            map.put(nums[i] , i);
+        }
+        throw new RuntimeException("array two num sum no target");
+    }
+
+//    public static int[] twoSum(int[] nums, int target) {
+//        for(int i=0;i<nums.length ;i++){
+//            for(int j=i+1 ; j <= nums.length ;j++){
+//                if(nums[j] == (target - nums[i])){
+//                    return new int[]{i+1,j+1};
+//                }
+//            }
+//        }
+//
+//        throw  new RuntimeException("array two sum  is no target");
+//
+//    }
+}
---
+++ src/main/java/com/inga/_2AddTwoNums.java
@@ -0,0 +1,63 @@
+package com.inga;
+
+import java.util.List;
+
+/**
+ * Created by abing on 2016-10-31.
+ */
+public class _2AddTwoNums {
+
+    public static void main(String[] args) {
+        ListNode t1 = new ListNode(3);
+        ListNode t2 = new ListNode(7);
+        ListNode t3 = new ListNode(4);
+        ListNode t4 = new ListNode(9);
+
+        ListNode l1 = new ListNode(9);
+        l1.next = t1;
+        l1.next.next = t2;
+
+        ListNode l2 = new ListNode(9);
+        l2.next = t4;
+        l2.next.next = t3;
+
+        ListNode ls = getAddListNode(l1 , l2);
+        while (ls != null) {
+            System.out.println(ls.val);
+            ls = ls.next;
+        }
+    }
+
+    public static ListNode getAddListNode(ListNode l1 , ListNode l2){
+        ListNode dummy = new ListNode(0);
+        ListNode p = l1 , q = l2 , curr = dummy;
+        int carry = 0;
+        while (p != null || q != null){
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = (x+y+carry);
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        if (carry > 0 ) {
+            curr.next = new ListNode(carry);
+        }
+
+        return dummy.next;
+    }
+}
+
+class ListNode{
+    int val ;
+    ListNode next;
+    public  ListNode(int x){
+        this.val = x;
+    }
+}
